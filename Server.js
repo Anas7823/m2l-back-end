fed8723d1e52ba2686b1a8b30efc1a3f85c6e5f4
William Mollet <william.mollet.en.uvsq.fr>
2022-12-15 21:58:39 +0100

    Route ListeProduit, AdminProduit et AdminUser

--- Server.js
+++ Server.js
@@ -27,6 +27,98 @@ app.get('/:sport', async(req, res) =>{
     res.status(200).json(rows)
 })
 
+app.get('/utilisateur', async(req, res) =>{
+    let conn;
+    console.log('Connexion')
+    conn = await mariadb.pool.getConnection();
+    console.log('Requète');
+    const rows = await conn.query('SELECT * FROM compte;')
+    console.log(rows);
+    res.status(200).json(rows)
+})
+
+app.get('/utilisateur/:id', async(req, res) =>{
+    let conn;
+    let id = req.params.id
+    console.log('Connexion')
+    conn = await mariadb.pool.getConnection();
+    console.log('Requète');
+    const rows = await conn.query('SELECT * FROM compte WHERE IdCompte = ?;',[id])
+    console.log(rows);
+    res.status(200).json(rows)
+})
+
+
+app.post('/utilisateur', async(req,res) =>{
+    let conn;
+    console.log('Connexion');
+    conn = await mariadb.pool.getConnection();
+    console.log('Requète');
+    await conn.query('INSERT INTO compte(NomCompte, MdpCompte, MailCompte) VALUES (?,?,?)',[req.body.nom,req.body.mdp,req.body.mail])
+    const rows = await conn.query('SELECT * FROM compte;')    
+    console.log('Requète effectué');
+    res.status(200).json(rows)
+})
+
+app.put('/utilisateur/:id',async(req,res)=>{
+    let conn;
+    let id = req.params.id
+    console.log('Connexion');
+    conn = await mariadb.pool.getConnection();
+    console.log('Requète');
+    await conn.query('UPDATE compte SET NomCompte = ?, MdpCompte = ?, CompteAdmin = ?, MailCompte = ? WHERE IdCompte = ?',[req.body.nom,req.body.mdp,req.body.admin,req.body.mail,id])
+    const rows = await conn.query('SELECT * FROM compte;')    
+    console.log('Requète effectué');
+    res.status(200).json(rows)
+})
+
+app.post('/produit',async(req,res)=>{
+    let conn;
+    console.log('Connexion');
+    conn = await mariadb.pool.getConnection();
+    console.log('Requète');
+    await conn.query('INSERT INTO produit(NomProduit, PrixProduit, StockProduit, IdSport) VALUES (?,?,?,?)',[req.body.nom,req.body.prix,req.body.stock,req.body.sport])
+    const rows = await conn.query('SELECT * FROM produit;')    
+    console.log('Requète effectué');
+    res.status(200).json(rows)
+})
+
+app.delete('/utilisateur/:id',async(req,res)=>{
+    let conn;
+    let id = req.params.id
+    console.log('Connexion');
+    conn = await mariadb.pool.getConnection();
+    console.log('Requète');
+    await conn.query('DELETE FROM compte WHERE IdCompte = ?',[id])
+    const rows = await conn.query('SELECT * FROM compte;')    
+    console.log('Requète effectué');
+    res.status(200).json(rows)
+})
+
+app.put('/produit/:id',async(req,res)=>{
+    let conn;
+    let id = req.params.id
+    console.log('Connexion');
+    conn = await mariadb.pool.getConnection();
+    console.log('Requète');
+    await conn.query('UPDATE produit SET NomProduit = ?, PrixProduit = ?, StockProduit = ?, IdSport = ? WHERE IdProduit = ?',[req.body.nom,req.body.prix,req.body.stock,req.body.sport,id])
+    const rows = await conn.query('SELECT * FROM produit;')    
+    console.log('Requète effectué');
+    res.status(200).json(rows)
+})
+
+app.delete('/produit/:id',async(req,res)=>{
+    let conn;
+    let id = req.params.id
+    console.log('Connexion');
+    conn = await mariadb.pool.getConnection();
+    console.log('Requète');
+    await conn.query('DELETE FROM produit WHERE IdProduit = ?',[id])
+    const rows = await conn.query('SELECT * FROM produit;')    
+    console.log('Requète effectué');
+    res.status(200).json(rows)
+})
+
 app.listen(3000, () =>{
     console.log("Serveur à l'écoute");
 } )
